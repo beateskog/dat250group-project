1c13b340c3191b5e87f5035858b3e42e3e01e9bb
Beate Skogvik <beate.skogvik@student.uib.no>
2023-10-04 16:57:24 +0200

    delete file

--- feedapp/src/main/java/no/hvl/dat250/feedapp/exeptions/PollNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package no.hvl.dat250.feedapp.exeptions;
-
-public class PollNotFoundException extends RuntimeException {
-    public PollNotFoundException(String message) {
-        super(message);
-    }
-} 
