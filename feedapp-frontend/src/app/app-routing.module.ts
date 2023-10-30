60e3442fdb0188315187f02e05cd1b83f208830b
ritaborlaug <rita.borlaug@gmail.com>
2023-10-30 17:05:43 +0100

    added css style to login and register

--- feedapp-frontend/src/app/app-routing.module.ts
+++ feedapp-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { OverviewComponent } from './overview/overview.component';
+import { MyPollsComponent } from './myPolls/myPolls.component';
+import { VoteComponent } from './vote/vote.component';
+import { ResultsComponent } from './results/results.component';
 
 
 const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'overview', component: OverviewComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'myPolls', component: MyPollsComponent},
+    { path: 'vote', component: VoteComponent},
+    { path: 'results', component: ResultsComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
