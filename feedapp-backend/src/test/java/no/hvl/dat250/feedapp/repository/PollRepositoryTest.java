754519b6d313160ef93d0787be17a255d31614e1
Beate Skogvik <beate.skogvik@student.uib.no>
2023-10-30 17:27:35 +0100

    iot working

--- feedapp-backend/src/test/java/no/hvl/dat250/feedapp/repository/PollRepositoryTest.java
+++ feedapp-backend/src/test/java/no/hvl/dat250/feedapp/repository/PollRepositoryTest.java
@@ -1,13 +1,77 @@
 package no.hvl.dat250.feedapp.repository;
 
+import no.hvl.dat250.feedapp.model.Account;
+import no.hvl.dat250.feedapp.model.Poll;
+import no.hvl.dat250.feedapp.model.Role;
+import no.hvl.dat250.feedapp.model.Vote;
+import no.hvl.dat250.feedapp.model.VotingPlatform;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import java.time.LocalDateTime;
 
-// There is no need to write unit test cases for the methods we are directly
-// using from the JPA repository (such as FingById), because the JPA repository
-// already has these methods implemented and tested.
 
+
+@DataJpaTest
 @ActiveProfiles("test")
 public class PollRepositoryTest {
+
+    @Autowired
+    private AccountRepository accountRepository;
+    Account account;
+
+    @Autowired
+    private PollRepository pollRepository;
+    Poll poll;
+
+    @Autowired
+    private VoteRepository voteRepository;
+    Vote vote;
+
+
+    @BeforeEach
+    public void setUp() {
+        account = new Account(1L, "Username1", "********", Role.ADMIN);
+        accountRepository.save(account);
+
+        poll = new Poll(
+            1L,
+            1234,
+            "poll.com",
+            "Do you like Christmas?",
+            LocalDateTime.of(2023, 10, 10, 19, 0),
+            LocalDateTime.of(2023, 12, 25, 20, 0));
+        poll.setAccount(account);
+        pollRepository.save(poll);
+        
+        vote = new Vote(
+            1L,
+            true,
+            VotingPlatform.WEB,
+            LocalDateTime.of(2023, 10, 10, 19, 45));
+        voteRepository.save(vote);
+    
+    }
+
+    @AfterEach
+    void tearDown() {
+        account = null;
+        accountRepository.deleteAll();
+
+        poll = null;
+        pollRepository.deleteAll();
+
+        vote = null;
+        voteRepository.deleteAll();
+    }
+
+   
+
+ 
 
     
 
